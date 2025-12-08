21ec6c03278d4ef19716765b4a567bc25bec71e3
prls59 <119790965+prls59@users.noreply.github.com>
2025-12-08 15:51:50 +0000

    Day 8: Playground

---
+++ 08 Playground/part1.py
@@ -0,0 +1,84 @@
+import os
+
+datafile = "input.txt"
+
+if datafile == "input.txt":
+    connections = 1000
+else:
+    connections = 10
+check = 3
+
+box_circuit = {}
+distances = {}
+circuit_size = {}
+
+NO_CIRCUIT = -1
+
+def distance(a, b):
+    x1, y1, z1 = a
+    x2, y2, z2 = b
+    return ((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2) ** 0.5
+
+def connect(a, b, circuit_id, circuit_size):
+    if box_circuit[a] == NO_CIRCUIT:
+        if box_circuit[b] == NO_CIRCUIT:
+            box_circuit[a] = circuit_id
+            box_circuit[b] = circuit_id
+            circuit_size[circuit_id] = 2
+            circuit_id += 1
+        else:
+            box_circuit[a] = box_circuit[b]
+            circuit_size[box_circuit[b]] += 1
+    else:
+        if box_circuit[b] == NO_CIRCUIT:
+            box_circuit[b] = box_circuit[a]
+            circuit_size[box_circuit[a]] += 1
+        else:
+            if box_circuit[a] != box_circuit[b]:
+                merge_circuits(box_circuit, a, b, circuit_size)
+    return circuit_id
+
+def merge_circuits(box_circuit, box1, box2, circuit_size):
+    c1 = box_circuit[box1]
+    c2 = box_circuit[box2]
+    if circuit_size[c1] < circuit_size[c2]:
+        from_c = c1
+        to_c = c2
+    else:
+        from_c = c2
+        to_c = c1
+    change_list = [box for box, circuit in box_circuit.items() if circuit == from_c]
+    for box in change_list:
+        box_circuit[box] = to_c
+    circuit_size[to_c] += circuit_size[from_c]
+    circuit_size[from_c] = 0
+
+# Build dictionary of junction box locations, and the circuit they're in (currently none)
+with open(os.path.dirname(os.path.abspath(__file__)) + "/" + datafile) as input:
+    for line in input:
+        row = line[0:-1].split(',')
+        box = (int(row[0]), int(row[1]), int(row[2]))
+        box_circuit[box] = NO_CIRCUIT
+
+# Build dictionary of distances from a to b (where a < b)
+box_list = list(box_circuit)
+box_list.sort()
+for a in range(len(box_list) -1):
+    for b in range(a + 1, len(box_list)):
+        distances[(box_list[a],box_list[b])] = distance(box_list[a],box_list[b])
+
+# Build list of junction box pairs in distance order
+nearby_boxes = sorted(distances, key=lambda k: distances[k])
+
+# create circuits
+circuit_id = 0
+for c in range(connections):
+    circuit_id = connect(nearby_boxes[c][0], nearby_boxes[c][1], circuit_id, circuit_size)
+
+# get check product
+circuit_list = sorted(circuit_size, key=lambda k: circuit_size[k], reverse=True)
+result = 1
+for n in range(check):
+    result *= circuit_size[circuit_list[n]]
+
+print('Result = ', result)
